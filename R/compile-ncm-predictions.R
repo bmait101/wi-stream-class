86f52597fd4103a00ab5ba6beb0b13249b250d87
bmait101 <bmaitland101@gmail.com>
2022-04-13 15:31:05 -0500

    add script for compiling NCM predictions from local MS Access database

---
+++ R/compile-ncm-predictions.R
@@ -0,0 +1,44 @@
+# Connect to and pull data from local MS Access database
+# Bryan M Maitland
+# April 2022
+
+# ==============================================================================
+
+# Workflow
+# - switch to 32-bit R (global options, then restart R)
+# - connect to MS Access database
+# - pull data
+# - save data
+# - switch back to 64-bit R
+
+
+# Switch to 32-bit R (MS Access is 32-bit) =====================================
+
+
+# Connect and load data ========================================================
+
+# packages
+library(RODBC)  
+
+# Open DB connection
+con <- odbcConnectAccess(
+  here::here("data","whd","DataHydro24k.mdb"))
+
+# Check available tables in WHD Plus
+sqlTables(con)
+
+# Pull tables
+whd_flow_temp <- sqlFetch(con, "flow_temperature")
+
+# Close DB connection
+odbcClose(con)
+
+# Save data ============================================
+
+saveRDS(whd_flow_temp, here::here("data", "whd_ncm_preds"))
+
+
+### END 32-bit R session ###
+
+
+# Switch to 32-bit R (MS Access is 32-bit) =====================================
